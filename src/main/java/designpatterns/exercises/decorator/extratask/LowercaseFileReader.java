e6c1071663462508a8dd7d29b2920c8ac6a604e2
Krystian Laskowski <krystian.laskowski.1@gmail.com>
2019-01-26 00:05:25 +0100

    Decorator #2

--- src/main/java/designpatterns/exercises/decorator/extratask/LowercaseFileReader.java
+++ src/main/java/designpatterns/exercises/decorator/extratask/LowercaseFileReader.java
@@ -1,20 +1,39 @@
 package designpatterns.exercises.decorator.extratask;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
-public class LowercaseFileReader {
+public class LowercaseFileReader extends FilterInputStream {
+
+    protected LowercaseFileReader(InputStream in) {
+        super(in);
+    }
+
     public static void main(String[] args) {
-        int c; try {
+        int c;
+        try {
             InputStream in =
-                        new BufferedInputStream(
-                                new FileInputStream("README.md"));
-            while((c = in.read()) >= 0) { System.out.print((char)c);
+                    new LowercaseFileReader(
+                    new BufferedInputStream(
+                            new FileInputStream("README.md")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
             }
             in.close();
         } catch (IOException e) {
-            e.printStackTrace(); }
+            e.printStackTrace();
+        }
+    }
+
+    public int read() throws IOException {
+        int c = super.read();
+        return (c == -1 ? c : Character.toLowerCase((char) c));
+    }
+
+    public int read(byte[] b, int offset, int len) throws IOException {
+        int result = super.read(b, offset, len);
+        for (int i = offset; i < offset + result; i++) {
+            b[i] = (byte) Character.toLowerCase((char) b[i]);
+        }
+        return result;
     }
 }
