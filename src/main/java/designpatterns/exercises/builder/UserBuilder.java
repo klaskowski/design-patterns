da598a71978da872a6cef2e6e2db0aafa8537288
Krystian Laskowski <krystian.laskowski.1@gmail.com>
2019-01-26 00:10:17 +0100

    Builder

--- src/main/java/designpatterns/exercises/builder/UserBuilder.java
+++ src/main/java/designpatterns/exercises/builder/UserBuilder.java
@@ -1,4 +1,27 @@
 package designpatterns.exercises.builder;
 
 public class UserBuilder {
+    User user;
+    public UserBuilder(String name, String surname) {
+        user = new User(name, surname);
+    }
+
+    public UserBuilder setAge(int age){
+        user.setAge(age);
+        return this;
+    }
+
+    public UserBuilder setPhone(String phone){
+        user.setPhone(phone);
+        return this;
+    }
+
+    public UserBuilder setAddress(String address){
+        user.setAddress(address);
+        return this;
+    }
+
+    public User build() {
+        return user;
+    }
 }
